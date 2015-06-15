f406d29acf4b6403c81b13734e96a77d8628a4ef
depietristefano <deietri.stefano97@gmail.com>
2015-06-15 12:32:07 +0200

    https://github.com/depietristefano/comunicazione_server_ardino.git

---
+++ test arduino dhcp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace test_arduino_dhcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(2020);
+            server.Start();
+            Console.WriteLine("avvio server");
+            TcpClient arduino = server.AcceptTcpClient();
+
+            Console.WriteLine("arduino connesso");
+
+
+            while (true)
+            {
+                NetworkStream flusso = arduino.GetStream();
+
+                byte[] buffer = new byte[arduino.ReceiveBufferSize];
+                flusso.Read(buffer, 0, (int)arduino.ReceiveBufferSize);
+
+                string s = Encoding.UTF8.GetString(buffer);
+                s = s.Substring(0, s.IndexOf("\0"));
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
